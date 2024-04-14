85a063cc28cb5e3fa4ed4cbd0c49e4aa8aa1ef0c
R. Arnanda Adi Wijanarko <r.arnandaadiwijanarko@gamil.com>
2024-04-14 14:02:10 +0700

    Kafka Consumer

---
+++ gen-20-java-springboot-kafka-consumer/src/main/java/com/example/kafkacob2/Kafkacob2Application.java
@@ -0,0 +1,13 @@
+package com.example.kafkacob2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafkacob2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafkacob2Application.class, args);
+	}
+
+}
